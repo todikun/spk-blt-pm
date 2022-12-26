b7799a880da4de2fcef7e0834650f6605c0608ff
todikun <todirahmat123@gmail.com>
2022-12-26 11:37:52 +0700

    +update dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,20 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Warga;
+use App\Models\{Warga, Aspek, Kriteria};
 use Carbon\Carbon;
 
 class DashboardController extends Controller
 {
     public function index()
-    {
-        $warga_month = Warga::whereMonth('periode', Carbon::now()->format('m'))
-            ->whereYear('periode', Carbon::now()->format('Y'))
-            ->where('is_validasi', true)->where('nilai_akhir', '<>', 0)->orderBy('nilai_akhir', 'DESC')->get();
-            
-        $warga_year = Warga::whereYear('periode', Carbon::now()->format('Y'))
-        ->where('is_validasi', true)->where('nilai_akhir', '<>', 0)->orderBy('nilai_akhir', 'DESC')->get();
+    {   
+        $warga = Warga::whereYear('periode', Carbon::now()->format('Y'))
+        ->where('is_validasi', true)->where('nilai_akhir', '<>', 0)->get()->count();
 
-        return view('pages.index', compact('warga_month', 'warga_year'));
+        $aspek = Aspek::get()->count();
+        $kriteria = Kriteria::get()->count();
+
+        return view('pages.index', compact('warga', 'aspek', 'kriteria'));
     }
 }
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -7,24 +7,32 @@
     <!-- row -->
     <div class="container-fluid">
         <div class="row">
-            <div class="col-lg-6 col-sm-6">
+            <div class="col-lg-4 col-sm-4">
                 <div class="card">
                     <div class="stat-widget-two card-body">
                         <div class="stat-content">
                             <div class="stat-text">Calon Penerima BLT Tahun ini </div>
-                            <div class="stat-digit">{{$warga_year->count()}} <span
-                                    class="text-black-50 text-sm">Orang</span></div>
+                            <div class="stat-digit">{{$warga}} <span class="text-black-50 text-sm">Orang</span></div>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="col-lg-6 col-sm-6">
+            <div class="col-lg-4 col-sm-4">
                 <div class="card">
                     <div class="stat-widget-two card-body">
                         <div class="stat-content">
-                            <div class="stat-text">Calon Penerima BLT Bulan ini</div>
-                            <div class="stat-digit">{{$warga_month->count()}} <span
-                                    class="text-black-50 text-sm">Orang</span></div>
+                            <div class="stat-text">Jumlah Aspek</div>
+                            <div class="stat-digit">{{$aspek}}</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-sm-4">
+                <div class="card">
+                    <div class="stat-widget-two card-body">
+                        <div class="stat-content">
+                            <div class="stat-text">Jumlah Kriteria</div>
+                            <div class="stat-digit">{{$kriteria}}</div>
                         </div>
                     </div>
                 </div>
